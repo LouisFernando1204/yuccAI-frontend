d99c8afb9f91a30eee6eb0866ca8d53e655edf77
Louis Fernando <ferlouis55@gmail.com>
2024-11-19 19:24:06 +0700

    Init ; Set up TailwindCSS ; Install Axios & React Router Dom

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen h-full w-full flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold underline text-center">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
