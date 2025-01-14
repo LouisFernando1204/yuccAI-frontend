541e0d43cd58e58aa03b61840a56d9d003d35bb6
yebology <139052533+yebology@users.noreply.github.com>
2025-01-14 23:03:49 +0700

    delete unique fact

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -29,21 +29,3 @@ export const informationOptions: InformationOption[] = [
       "📚 Organisasi di UC apa saja sih yang seru? Lagi pingin tahu lebih banyak nih.",
   },
 ];
-
-export const uniqueFacts = [
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra memiliki banyak partner kerjasama, yang salah satunya adalah Apple Developer Academy!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa apabila kamu menjadi salah satu peserta program Apple Developer Academy, kamu akan berkesempatan untuk mendapatkan Iphone series terbaru serta Macbook series terbaru!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra adalah kampus yang mengedepankan prinsip Entrepreneurship. Jadi apapun jurusan yang kamu pilih nantinya, kamu akan tetap berkesempatan untuk belajar mengenai Entrepreneurship kok!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Apabila kamu memiliki passion lebih terhadap dunia content creator, kamu bisa memiliki kesempatan untuk bergabung menjadi salah satu UC Social Crew Member loh!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Sebanyak 36 mahasiswa Universitas Ciputra pada tahun 2024 lalu berhasil menjadi peserta program MSIB loh!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa ada program bernama “Family Business” di Universitas Ciputra untuk mempersiapkan mahasiswa-mahasiswa yang tergabung untuk meneruskan bisnis keluarga.",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? IISMA Awardees dari Universitas Ciputra dari tahun ke tahun selalu mengalami kenaikan, tepatnya di 2021 sebanyak 10 mahasiswa, 2022 sebanyak 17 mahasiswa, 2023 sebanyak 23 mahasiswa, dan 2024 adalah sebanyak 29 mahasiswa!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Penilaian di Universitas Ciputra mayoritas merupakan project based. Jadi kamu bisa dapat kesempatan untuk belajar praktek lebih banyak daripada sekedar teori saja!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa di Universitas Ciputra ada UC Venture yang merupakan program inkubator yang membuka kesempatan untuk kamu yang ingin mengembangkan bisnis lebih lanjut!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra merupakan perguruan tinggi swasta nomor 1 di Jawa Timur, nomor 3 di Indonesia, dan nomor 12 di ASEAN berdasarkan Applied HE loh!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra menyediakan tempat konseling yang bernama UC Counseling Buddy untuk mahasiswa yang membutuhkan konsultasi psikologis.",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra mendapatkan 5 bintang untuk kategori fasilitas, pembelajaran, inklusivitas, pengembangan akademik, dan employability berdasarkan QS Asia University Ranking.",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra memiliki lebih dari 170 partner nasional dan 70 lebih partner internasional loh!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra adalah kampus swasta nomor 1 di Indonesia yang berhasil meloloskan mahasiswa terbanyak untuk program IISMA 2023 loh!",
-  "Sambil menunggu jawaban dari pertanyaanmu, yuk simak fakta unik dari kampus ini! Tahukah kamu? Bahwa Universitas Ciputra adalah kampus swasta termuda dan tercepat di Jawa Timur yang berhasil meraih Akreditasi Unggul loh!"
-];
